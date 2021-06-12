fb47816e459c230d7a8063ec34b0f149e834cfd6
Vitor Araujo <vitorgamer58@outlook.com>
2021-06-12 12:18:03 -0300

    Added Asserts

--- test/index.js
+++ test/index.js
@@ -1,4 +1,9 @@
 const Gracchi = require("../dist").default;
+require('dotenv').config()
+var expect = require('chai').expect
+  , foo = 'bar'
+  , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+
 
 const base = "BNB",
   quote = "CBRL";
@@ -16,17 +21,27 @@ function delay(interval) {
 
 describe("account", () => {
   it("should receive balances", async () => {
-    await api.balance();
+    const balance = await api.balance();
+    balance.forEach(body => {
+      expect(body).to.have.all.keys('name', 'deposit', 'available', 'onHold', 'symbol', 'withdrawal')
+    });
   });
 });
 
 describe("market", () => {
   it("should receive orderbook", async () => {
-    await api.orderbook({ base, quote });
+    await api.orderbook({ base, quote }).then((res) => {
+      expect(res).to.have.all.keys('asks', 'bids')    
+    })
+
   });
 
   it("should receive ticker", async () => {
-    await api.ticker({ base, quote });
+    const ticker = await api.ticker({ base, quote }).then((res) => {
+      expect(res).to.have.all.keys('base', 'quote', 'change', 'last', 'vol', 'high', 'low', 'ask', 'bid', 'askAmount', 'bidAmount')
+      expect(res.base).to.be.equal(base)
+      expect(res.quote).to.be.equal(quote)
+    })
   });
 });
 
@@ -51,10 +66,17 @@ describe("order flow", () => {
   delay(1000);
 
   it("should receive order", (done) => {
-    api.getOrder({ orderId }).then(() => done());
+    api.getOrder({ orderId }).then((res) => {
+      expect(res).to.have.all.keys('status', 'averagePrice', 'filledAmount', '_id', 'base', 'quote', 'side', 'price', 'originalAmount', 'remainingAmount', 'fee')
+      expect(res._id).to.be.equal(orderId)
+      done()
+    });
   });
 
   it("should cancel order", (done) => {
-    api.cancelOrder({ orderId }).then(() => done());
+    api.cancelOrder({ orderId }).then((res) => {
+      expect(res.orderId).to.be.equal(orderId)
+      done()
+    });
   });
 });
